0facc7477cf3e43371207bbfd1d2b71947395a7f
Douglas Berkley <douglasmberkley@gmail.com>
2025-11-19 10:19:13 +0900

    added chat history and turbo_stream views

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,6 +1,12 @@
 class ChallengesController < ApplicationController
   def index
     @challenges = Challenge.all
+    # render 'index.html.erb'
+    # respond_to do |format|
+    #   format.html { render 'index' }
+    #   format.json { render json: @challenges }
+    #   format.txt { render plain: @challenges }
+    # end
   end
 
   def show
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,12 +9,22 @@ class MessagesController < ApplicationController
     @message.role = "user"
 
     if @message.save
-      ruby_llm_chat = RubyLLM.chat
-      response = ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+      @ruby_llm_chat = RubyLLM.chat
+      # give the ruby_llm_chat the history of our conversation
+      build_conversation_history
+      response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
       @chat.generate_title_from_first_message
 
-      redirect_to chat_messages_path(@chat)
+      # redirect_to chat_messages_path(@chat)
+      # stay on the page, display the two new messages only
+      # find the messages div
+      # insert the last two messages into that
+      # render create.turbo_stream.erb
+      respond_to do |format|
+        format.html { redirect_to chat_messages_path(@chat) }
+        format.turbo_stream { render 'create' }
+      end
     else
       render "chats/show", status: :unprocessable_entity
     end
@@ -22,6 +32,13 @@ class MessagesController < ApplicationController
 
   private
 
+  def build_conversation_history
+    @chat.messages.each do |message|
+      @ruby_llm_chat.add_message(message)
+    end
+  end
+
+  # system prompt, challenge context + message i sent (send the chat history)
   def instructions
     challenge_context = "Here is the context of the challenge: #{@challenge.content}."
     [SYSTEM_PROMPT, challenge_context, @challenge.system_prompt].compact.join("\n\n")
